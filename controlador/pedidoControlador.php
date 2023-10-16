731a50040f8bb6a98f165955b20ff3037c4813b6
Marc <marcmartinezsofp@bernatelferrer.cat>
2023-10-16 19:31:09 +0200

    Hola

---
+++ controlador/pedidoControlador.php
@@ -0,0 +1,12 @@
+<?php
+    //Crearemos el controlador de pedidos.
+    class pedidoControlador{
+        public function index(){
+            echo 'Pagina principal pedidos.';
+
+        }
+        public function compra(){
+            echo 'Pagina de compra.';
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <?php
+/*
     if(isset($_GET['controlador'])) {
         echo 'Quieres realizar una acción sobre '.$_GET['controller'];
         if(isset($_GET['action'])) {
-            echo 'Sobre '.$_GET['controlador']. 'quieres mostrar la pagina '.$_GET['action'];
+            echo 'Sobre '.$_GET['controlador']. ' quieres mostrar la pagina '.$_GET['action'];
         }
     } else {
         echo 'No me has pasado controller.';
     }
+*/
+    include_once 'controlador/pedidoControlador.php';
+    
+    if(isset($_GET['controlador'])){
+        //Si no pasa nada se mostrará pagina principal pedidos.
+    } else {
+        $nombre_controlador = $_GET['controlador'].'controlador';
 
+        if(class_exists($nombre_controlador)){
+            $controlador = new $nombre_controlador(); 
+            
+            //Miramos si nos pasa una acción, en caso contrario mostraremos acción por defecto.
+            
+            if(isset($_GET['action'])){
+                $action = $_GET['action'];
+            }
+            //echo 'Quieres visualizar una acción de '.$nombre_controlador;
+        } else {
+            $action = 'index';
+            //echo $nombre_controlador.' no existe.';
+        }
+        
+        $controlador->$action();
+    }
 ?>
