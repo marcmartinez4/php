249196d9412bb2fd0221fc7f8bcba69a30684ba1
Marc <marcmartinezsofp@bernatelferrer.cat>
2023-10-18 19:25:48 +0200

    Hola

--- modelo/ProductoDAO.php
+++ modelo/ProductoDAO.php
@@ -1,9 +1,19 @@
 <?php
-
     include_once 'config/dataBase.php';
+    
     class ProductoDAO {
         public static function getAllProducts() {
             $con = dataBase::connect();
+            if($result = $con->query("SELECT * FROM product")) {
+                while($producto = $result->fetch_array()) {
+                    echo $producto['name']->getName();
+                    echo '<br>';
+                }
+            }
+            /*$stmt = $con ->prepare("SELECT * FROM producto");
+            $stmt->execute();
+            var_dump($stmt->get_result());
+            */
         }
     }
 
